c2e525862efcaabd6a41ca7025fd47d842e417b8
Addo Michael <119521479+codingaddo@users.noreply.github.com>
2023-11-15 01:04:27 +0000

    .

---
+++ app/components/Mesages.jsx
@@ -0,0 +1,27 @@
+'use client'
+import React from 'react'
+import styles from '../styles/mesages.module.css'
+
+const Mesages = () => {
+  return (
+    <>
+    <div className={styles.toggle}>X</div>
+    <div className={styles.container}>
+        <div className={styles.stepContainer}>
+            <span className={styles.active}>1</span>
+            <span className={styles.steps}>2</span>
+            <span className={styles.steps}>3</span>
+        </div>
+        <div className={styles.message}>
+            Messages wouold be displayed here gggggggggggggggggggggggggggggggggggggg
+        </div>
+        <div className={styles.btnContainer}>
+            <button className={styles.btn}>Previous</button>
+            <button className={styles.btn}>Next</button>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Mesages
