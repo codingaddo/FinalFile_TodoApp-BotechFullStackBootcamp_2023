2fe76a077f9e2a6e2f833e3dc13a894337a0acc5
Addo Michael <119521479+codingaddo@users.noreply.github.com>
2023-11-17 23:15:30 +0000

    Update Mesages.jsx

--- app/components/Mesages.jsx
+++ app/components/Mesages.jsx
@@ -1,5 +1,4 @@
-'use client'
-import React, { useState } from 'react'
+import React from 'react'
 import styles from '../styles/mesages.module.css'
 
 const data = [
@@ -9,36 +8,25 @@ const data = [
 ]
 
 const Mesages = () => {
-  const [step,setStep] = useState(1)
-  const [isOpen,setIsOpen] = useState(true)
-
-  const next =()=>{
-    if(step!=3) setStep(step+1)
-    
-  }
+ 
   return (
     <>
-    <div className={styles.toggle} onClick={()=>setIsOpen(!isOpen)} >{isOpen===true?'X':'M'}</div>
-    {
-      isOpen &&
-
+    <div className={styles.toggle}  >X</div>
     <div className={styles.container}>
         <div className={styles.stepContainer}>
-            <span className={step===1?styles.active:styles.steps}>1</span>
-            <span className={step===2?styles.active:styles.steps}>2</span>
-            <span className={step===3?styles.active:styles.steps}>3</span>
+            <span className={styles.active}>1</span>
+            <span className={styles.steps}>2</span>
+            <span className={styles.steps}>3</span>
         </div>
         <div className={styles.message}>
-            Step:{step} {data[step-1]}
+            Step:1 Meassges would be displayed here
         </div>
         <div className={styles.btnContainer}>
-            <button className={styles.btn} onClick={()=>{if(step!=1)setStep(step-1)}} >Previous</button>
-            <button className={styles.btn} onClick={next} >Next</button>
+            <button className={styles.btn} >Previous</button>
+            <button className={styles.btn} >Next</button>
         </div>
     </div>
    
-
-    }
     </>
   )
 }
